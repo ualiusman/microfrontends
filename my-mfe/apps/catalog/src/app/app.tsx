2135fdda3936f993620fb2ffff460f6bf221f842
ualiusman <usmanaliua.uali@gmail.com>
2023-11-02 23:51:33 +0500

    product mocks

--- my-mfe/apps/catalog/src/app/app.tsx
+++ my-mfe/apps/catalog/src/app/app.tsx
@@ -2,12 +2,14 @@
 import { Header } from '@ebuy/ui';
 import { Container, Header as Text } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
+import ProductList from './ProductList/product-list';
 export function App() {
   return (
     <>
       <Container style={{ marginTop: '5rem' }}>
         <Header />
         <Text size="huge">Catalog App</Text>
+        <ProductList />
       </Container>
     </>
   );
